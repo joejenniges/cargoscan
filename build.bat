edf4daadacb2fef1a082edb47aaebee905000472
GunfighterJ <joseph.jenniges@gmail.com>
2017-07-15 13:02:51 -0500

    fix build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-python C:\Python27\pyinstaller\pyinstaller.py --onefile --noconsole --icon=resources\img\icon.ico --name=CargoScan  src\main.py
+python C:\Python27\pyinstaller\pyinstaller.py --onefile --noconsole --icon=resources\ui\img\icon.ico --name=CargoScan  src\main.py
